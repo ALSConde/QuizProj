19b7004394095011b97f369abe03adff6c0e8de2
André Conde <andre.conde@aluno.ifsp.edu.br>
2023-01-12 14:22:30 -0300

    Projeto Final

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# QuizProj
+# ProjetoBuzzfeed
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.2.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/quiz-proj'),
+      dir: require('path').join(__dirname, './coverage/projeto-buzzfeed'),
       subdir: '.',
       reporters: [
         { type: 'html' },
